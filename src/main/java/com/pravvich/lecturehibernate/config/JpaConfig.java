218db0d994a0c2e7c9b5f201991dd1787d10c556
Pavel Ravvich <triodjangopiter@yandex.ru>
2020-11-09 00:56:09 +0300

    re

--- src/main/java/com/pravvich/lecturehibernate/config/JpaConfig.java
+++ src/main/java/com/pravvich/lecturehibernate/config/JpaConfig.java
@@ -1,25 +1,16 @@
 package com.pravvich.lecturehibernate.config;
 
-import com.pravvich.lecturehibernate.model.Customer;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AvailableSettings;
-
-import org.hibernate.internal.SessionFactoryImpl;
-import org.hibernate.jpa.boot.spi.EntityManagerFactoryBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.Properties;
 
