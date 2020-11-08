ac04cb6a69e1dbe0e511642b8a6a4c2ea6d979a6
Pavel Ravvich <triodjangopiter@yandex.ru>
2020-11-08 03:57:53 +0300

    initial commit

---
+++ src/main/java/com/pravvich/lecturehibernate/LectureHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.pravvich.lecturehibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LectureHibernateApplication.class, args);
+	}
+
+}
