7999bcb85b965be07f68048c5a6b15ed3da111e4
Pavel Ravvich <triodjangopiter@yandex.ru>
2020-11-09 04:56:09 +0300

    jpa conf

--- src/main/java/com/pravvich/lecturehibernate/config/JpaConfig.java
+++ src/main/java/com/pravvich/lecturehibernate/config/JpaConfig.java
@@ -1,56 +1,46 @@
 package com.pravvich.lecturehibernate.config;
 
+import lombok.AllArgsConstructor;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.AvailableSettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
-import java.util.Properties;
 
 
 @Configuration
+@AllArgsConstructor
 @EnableTransactionManagement
 @EnableJpaRepositories("com.pravvich")
 @PropertySource("classpath:application.properties")
 public class JpaConfig {
 
-    @Bean
-    public LocalSessionFactoryBean sessionFactory(DataSource dataSource) {
-        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource);
-        sessionFactory.setPackagesToScan("com.pravvich");
+    private final DataSource dataSource;
 
-        Properties properties = new Properties();
-        properties.put(AvailableSettings.SHOW_SQL, true);
-        properties.put(AvailableSettings.STATEMENT_BATCH_SIZE, 10);
-        properties.put(AvailableSettings.HBM2DDL_AUTO, "validate");
-        properties.put(AvailableSettings.DIALECT, "org.hibernate.dialect.PostgreSQL94Dialect");
-        sessionFactory.setHibernateProperties(properties);
-        return sessionFactory;
+    private final JpaVendorAdapter jpaVendorAdapter;
+
+    @Bean
+    @Primary
+    public EntityManagerFactory entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setDataSource(dataSource);
+        emf.setJpaVendorAdapter(jpaVendorAdapter);
+        emf.setPackagesToScan("com.pravvich.lecturehibernate");
+        emf.setPersistenceUnitName("default");
+        emf.afterPropertiesSet();
+        return emf.getObject();
     }
 
     @Bean
-    HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
-        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-        transactionManager.setSessionFactory(sessionFactory);
-        return transactionManager;
-    }
-
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.ds.PGSimpleDataSource");
-        dataSource.setUrl("jdbc:postgresql://127.0.0.1:5432/hibernate_lecture");
-        dataSource.setUsername("app");
-        dataSource.setPassword("pas");
-        return dataSource;
+    public SessionFactory setSessionFactory(EntityManagerFactory entityManagerFactory) {
+        return entityManagerFactory.unwrap(SessionFactory.class);
     }
 
 }
